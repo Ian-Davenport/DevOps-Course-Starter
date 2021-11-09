50f280b512a1efc44af39204ee1ab575f0b8a3f5
Ian Davenport <ian.davenport@kpmg.co.uk>
2021-11-09 14:50:22 +0000

    Lists saved items. Next: Add an item

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask import render_template
-
-
+from todo_app.data.session_items import get_items
 from todo_app.flask_config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -10,13 +10,18 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = get_items()
+    return render_template('index.html', get_items=get_items)
+    
+    
 
-
-
+@app.route('/todo', methods=['GET', 'POST'])
+def add_todo():
+   return render_template('todo.html', get_items=get_items)
+ 
 
 # TASKS TO DO:
 # Make the app render the index page HTML template.        >>>>>  DONE
-# Display the list of saved todo items on the index page. 
-# Create a route to add a new todo item to the list.
+# Display the list of saved todo items on the index page.  >>>>>  DONE
+# Create a route to add a new ToDo item to the list.       >>>>>  DONE ish 
 
