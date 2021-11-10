92af79338daa54bea6cde5fa7b9b6a7849eff945
Ian Davenport <ian.davenport@kpmg.co.uk>
2021-11-10 13:13:17 +0000

    Test commit

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import render_template
-from todo_app.data.session_items import get_items
+from flask import render_template, redirect, request
+from flask.helpers import url_for
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 
@@ -8,20 +9,16 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', method=['GET'])
 def index():
     items = get_items()
     return render_template('index.html', get_items=get_items)
-    
-    
 
-@app.route('/todo', methods=['GET', 'POST'])
-def add_todo():
-   return render_template('todo.html', get_items=get_items)
- 
 
-# TASKS TO DO:
-# Make the app render the index page HTML template.        >>>>>  DONE
-# Display the list of saved todo items on the index page.  >>>>>  DONE
-# Create a route to add a new ToDo item to the list.       >>>>>  DONE ish 
+@app.route('/todo', methods=['POST'])
+def add_todo():  
+    add_item(title=request.form.get('item_name'))
+    return redirect(url_for('index'))  
+
+    
 
